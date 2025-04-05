9ed5ee8b63fd9f3571d078d530be89265f662b04
Hoarfroster <hoarfroster@outlook.com>
2025-04-05 19:45:28 +0800

    fix styles; basic model

--- src/pages/dashboard-activity.vue
+++ src/pages/dashboard-activity.vue
@@ -1,9 +1,28 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { setTitle } from '@utils/title';
+import { getStorageItem } from '@utils/storage';
+import { UserType } from '@models/user';
+import Tabs from '@components/tabs.vue';
+import { ref } from 'vue';
 
 setTitle('活动管理页');
+
+const tab = ref(0);
+
+const userType = JSON.parse(getStorageItem('admin') ?? '{}').type,
+    tabs: string[] = [];
+
+switch (userType) {
+    case UserType.UserOrg:
+    case UserType.UserLocalOrg:
+        tabs.push('加分条管理', '活动事项管理');
+        break;
+}
 </script>
 
 <template>
-    <h1 class="text-3xl font-black">活动管理页</h1>
+    <div class="mx-auto w-full px-4">
+        <Tabs v-model:tab="tab" :tabs="tabs" />
+
+    </div>
 </template>
